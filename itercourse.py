865a01745d0b0017fad0660a634e6024a70f8e23
cterdam <llz.joshlee@gmail.com>
2020-10-17 20:18:45 +0800

    SOC now can parse info, TODO parse instructors and description

--- itercourse.py
+++ itercourse.py
@@ -2,8 +2,8 @@
 itercourse
 Iterators that take in SOC/Spreadsheet and output courses.
 """
-import course
 import re
+import course
 
 
 class SOCIter():
@@ -14,45 +14,69 @@ class SOCIter():
         self.lines = soc_lines
         self.i = 0
 
+        # For regex matching purpose
+        self.header_pattern = \
+            r'(?P<number>98\d{3})'\
+            r' Student Taught Courses \(StuCo\): '\
+            r'(?P<long_title>.+)'\
+            r' \('\
+            r'(?P<short_title>STUCO: .+)'\
+            r'\)'
+
+        # Skip 98000
+        while not re.match(self.header_pattern, self.lines[self.i]):
+            self.i += 1
+            if self.i >= len(self.lines):
+                raise StopIteration
+        self.i += 1
+
     def __iter__(self):
         return self
 
     def __next__(self):
-        while not self.parse_course_header(self.i):
+        # Find next course header line
+        while not re.match(self.header_pattern, self.lines[self.i]):
             self.i += 1
-            if self.over_bound(self.i):
+            if self.i >= len(self.lines):
                 raise StopIteration
-        # i currently pointing at a header
 
-        thisCourse = course.Course()
+        this_course = course.Course()
 
-        # Reading from header: number, long title and short title
-        header_match = self.parse_course_header(self.i)
-        header_dict = header_match.groupdict()
-        thisCourse.number = header_dict["number"]
-        thisCourse.long_title = header_dict["long_title"]
-        thisCourse.short_title = header_dict["short_title"]
+        # Add number, long title and short title to course
+        header_dict = re.match(self.header_pattern,
+                               self.lines[self.i]).groupdict()
+        this_course.number = header_dict["number"]
+        this_course.long_title = header_dict["long_title"]
+        this_course.short_title = header_dict["short_title"]
 
-
+        # Build info pattern from info header
         self.i += 1
-        return thisCourse
+        info_pattern = self.prepare_info_pattern(self.lines[self.i])
 
+        # Parse info line with info header and add info to course
+        self.i += 1
+        info_dict = re.match(info_pattern, self.lines[self.i]).groupdict()
 
+        return this_course
 
-    def parse_course_header(self, idx):
+    def prepare_info_pattern(self, info_header):
         """
-        Returns a regex match object if the string at index idx of the
-        lines seems to be a valid course header. None otherwise.
+        Given the information header string, prepare a regex pattern
         """
-        curr = self.lines[idx]
-        header_pattern = \
-                r'(?P<number>98\d{3})'\
-                r' Student Taught Courses \(StuCo\): '\
-                r'(?P<long_title>.*)'\
-                r' \('\
-                r'(?P<short_title>STUCO: .*)'\
-                r'\)'
-        return re.match(header_pattern, curr)
-
-    def over_bound(self, i):
-        return self.i >= len(self.lines)
+        buildup_list = \
+            [('LEC/SEC', r'[A-Z0-9]+', 'lecture'),
+             ('DAY(S)', r'(M|T|W|R|F|S|U|,)+', 'day_of_week'),
+             ('BEGIN TIME', r'\d{2}:\d{2}(A|P)M', 'begin_time'),
+             ('END TIME', r'\d{2}:\d{2}(A|P)M', 'end_time'),
+             ('BLDG/ROOM', r'[\w ]+', 'location'),
+             ('INSTRUCTOR(S)', r'[A-Z]\w*,\s+[A-Z]\s+\(\w+\);*.*',
+                 'instructors'),
+             ('MAX ENROLL', r'[0-9]+', 'max_enroll'),
+             ('ACT ENROLL', r'[0-9]+', 'act_enroll'),
+             ('WL SIZE', r'[0-9]+', 'wl_size'),
+             ('LOCATION', r'\w+', 'campus')]
+        buildup_list.sort(key=lambda x: info_header.find(x[0]))
+        buildup_list = ['(?P<' + x[2] + '>' + x[1] + ')' for x in buildup_list]
+        buildup_list = [buildup_list[0]] + \
+            [r'\s+' + x for x in buildup_list[1:]]
+        return "".join(buildup_list)
